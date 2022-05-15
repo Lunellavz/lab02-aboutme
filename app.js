afedecd8b5f8e19eace4d2233bf8f1ac3a2f183a
Gabriella <gabi.andino2000@gmail.com>
2022-05-14 17:40:58 -0700

    merged lab 03 and 04

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 'use strict';
 
+<<<<<<< Updated upstream
 function Questions(answer) {
  answer=answer.toLowerCase();
  if (answer==="yes" || answer==="y"){
@@ -80,3 +81,66 @@ switch (cook ?.toLowerCase()) {
         break;   
 }
 
+=======
+// let userName = prompt("What is your name?");
+// document.write ("Hello " + userName + ", welcome to my page!");
+
+// let farm = prompt("Do I want a farm?")?.toLowerCase();
+// if (farm === "yes" || farm === "y") {
+//     alert("Correct!");
+// } else if (farm === "no" || farm === "n") {
+//     alert("FALSE!! I said it like 3 times...");
+// } else {
+//   alert("(Y)es or (N)o");
+// }
+
+// let favColor = prompt("Is my favorite color purple?")?.toLowerCase();
+// if (favColor === "yes" || favColor ==="y") {
+//     alert("Nope I fooled you!"); 
+// } else if (favColor === "no" || favColor === "n") {
+//     alert("My favorite color is Red!");
+// }
+
+// let cows = prompt("Do I want cows on my farm?")?.toLowerCase();
+// if (cows === "yes" || cows === "n") {
+//     alert("Of course!");
+// }else if(cows === "no" || cows === "n") {
+//     alert("Why wouldn't I want those cuties?");
+// }
+
+// let travel = prompt("Do I only want to stay in Seattle?")?.toLowerCase();
+// if (travel === "yes"|| travel === "y") {
+//     alert("That sounds miserable");
+// } else if (travel === "no" || travel === "n") {
+//     alert("I want to see the world through my own eyes!");
+// }
+
+// let cook = prompt("Can I cook");
+// switch (cook ?.toLowerCase()) {
+//     case "y":
+//     case "yes":
+//         alert("Yes, I love to cook");
+//         break;
+//     case "n":
+//     case "no":
+//         alert("Incorrect, I love to cook");
+//         break;   
+// }
+
+const tenNames = ["Pizza", "Iced matcha latte", "Rootbeer", "PB cookies"];
+console.log(tenNames);
+
+const Ggames = prompt("Name one of my favorite drinks or foods.");
+
+for (let i = 0 ; i < tenNames.length; i++) {
+    const tenName = tenNames[i];
+
+    console.log(i, Ggames);
+
+    if (Ggames == tenName) {
+        console.log("Found", Ggames);
+        alert(tenName + " is so good!");
+    }
+}
+
+>>>>>>> Stashed changes
